a718675502ee6c9ac91e207d42ee329c02458682
Sag05 <samuel.grenehed@gmail.com>
2021-12-09 16:09:20 +0100

    Added d4p1

---
+++ day4part1/day4part1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace day4part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input;
+            input = File.ReadLines("input.txt").ToArray();
+            string[] numbers = input[0].Split(',');
+            int numberOfBoards = (input.Length - 1) / 6;
+            string[,,] boards = new string[numberOfBoards, 5, 5];
+
+
+            for (int i = 0; i < numberOfBoards; i++)
+            {
+                for (int y = 0; y < 5; y++)
+                {
+                    string[] ySplit = 
+
+                    for (int x = 0; x < 5; x++)
+                    {
+                        boards[i,y,x] = 
+                    }
+                }
+            }
+
+
+
+        }
+    }
+}
